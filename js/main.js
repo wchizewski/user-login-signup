edad63957ef5324f3e3370d8c822f224e7f7a5d5
wchizewski <90284175+wchizewski@users.noreply.github.com>
2022-10-06 14:48:58 -0600

    begun

--- js/main.js
+++ js/main.js
@@ -1,19 +1,40 @@
 // USER LOGIN / SIGNUP
 
 // HTML VARIABLES
-let signInBtn = document.getElementById('sign-in-btn');
-let signUpBtn = document.getElementById('sign-up-btn');
+let signInBtn = document.getElementById("sign-in-btn");
+let signUpBtn = document.getElementById("sign-up-btn");
 
 // SIGN UP BTN CLICKED
-signUpBtn.addEventListener('click', signUpHandler);
+signUpBtn.addEventListener("click", signUpHandler);
+
+let users = loadUsers();
 
 function signUpHandler() {
-  console.log('Sign Up Btn Clicked');
+  let username = document.getElementById("password").value;
+  let password = document.getElementById("password").value;
+  let passwordConfirm = document.getElementById("passwordConfirm").value;
 }
 
 // SIGN IN BTN CLICKED
-signInBtn.addEventListener('click', signInHandler);
+signInBtn.addEventListener("click", signInHandler);
 
 function signInHandler() {
-  console.log('Sign In Btn Clicked');
+  console.log("Sign In Btn Clicked");
+}
+
+// helper
+function newUser(username, password) {
+  return {
+    username: username,
+    password: password,
+  };
+}
+
+function loadUsers() {
+  let usersStr = localStorage.getItem("users");
+  return JSON.parse(usersStr) ?? [];
+}
+
+function saveUsers() {
+  localStorage.setItem("users", JSON.stringify(users));
 }
