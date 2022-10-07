1165bc581f0569047a0a2659067eeba6c931b636
wchizewski <90284175+wchizewski@users.noreply.github.com>
2022-10-07 14:18:20 -0600

    friday

--- js/main.js
+++ js/main.js
@@ -7,12 +7,23 @@ let signUpBtn = document.getElementById("sign-up-btn");
 // SIGN UP BTN CLICKED
 signUpBtn.addEventListener("click", signUpHandler);
 
-let users = loadUsers();
+let users = JSON.parse(localStorage.getItem("users")) ?? [];
+// loadUsers();
 
 function signUpHandler() {
   let username = document.getElementById("password").value;
   let password = document.getElementById("password").value;
   let passwordConfirm = document.getElementById("passwordConfirm").value;
+  if (passwordConfirm === password) {
+    alert("Sign up successful!");
+    users.push(newUser(username, password));
+    saveUsers();
+  }
+  for (let i = 0; i < users.length; i++) {
+    if (username === users[i]) {
+      alert("Username already in use");
+    }
+  }
 }
 
 // SIGN IN BTN CLICKED
@@ -30,10 +41,10 @@ function newUser(username, password) {
   };
 }
 
-function loadUsers() {
-  let usersStr = localStorage.getItem("users");
-  return JSON.parse(usersStr) ?? [];
-}
+// function loadUsers() {
+// let usersStr = localStorage.getItem("users");
+// return JSON.parse(usersStr) ?? [];
+// }
 
 function saveUsers() {
   localStorage.setItem("users", JSON.stringify(users));
